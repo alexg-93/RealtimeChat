1753392513d636c77f5bbad7f3c469020f262225
gordienko1993 <alexgordienko1993@gmail.com>
2020-10-08 16:34:26 +0300

    Realtime chat project

---
+++ chat.js
@@ -0,0 +1,57 @@
+//adding new chat documents
+//setting up a real-time listener to get new chats
+//update te username
+//updating the room
+
+
+class Chatroom{
+    constructor(room,username){
+        this.room=room;
+        this.username=username;
+        this.chats = db.collection('chats');
+        this.unsubscribe;
+
+    }
+    
+    async addChat(message){
+        //format char object
+        const now = new Date();
+        const chat = {
+            message:message,
+            username:this.username,
+            room:this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+       //save the chat document
+       const response = await this.chats.add(chat);
+       return response;
+    }
+
+     getChats(callback){
+        this.unsubscribe = this.chats
+        .where('room','==',this.room)
+        .orderBy('created_at')
+        .onSnapshot(snapshot=>{
+            snapshot.docChanges().forEach(change=>{
+             if(change.type==='added'){
+                 //update ui
+                 callback(change.doc.data());
+             }
+            });
+        });
+    }
+
+    updateName(username){
+        this.username=username;
+        localStorage.setItem('username',username)
+    }
+    updateRoom(room){
+      this.room = room;
+      console.log('room updated')
+      if(this.unsubscribe){
+        this.unsubscribe()
+      }
+    }
+
+}
+
